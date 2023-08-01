c5143babcd10b34c1f5e734071b4c8a39695982a
Mihai <44536334+kashyl@users.noreply.github.com>
2023-08-01 05:38:03 +0100

    Add progress when generating. Renamed files.

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-import sd_func
-from tagging_func import generate_tags
+import sd_api_wrapper as sd_api_wrapper
+from wd14_tagger_runner import generate_tags
 from config import SDModels, IMG_DIR
+import os, asyncio
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow debug & info (build for CPU optimization etc.)
 
 undesired_tags = '1girl,1boy,solo,no humans'
 
+# ratings: general, sensitive, questionable, explicit
+# todo: filtering (only general allowed)
 
 
 def main():
-    # sd_func.generate(sd_model=SDModels.sardonyxREDUX_v20, prompt='whale on deep blue sky,white birds,star,thick clouds')
-    generate_tags(
-        train_data_dir=IMG_DIR,
-        caption_extension='.json',
-        batch_size=8,
-        general_threshold=0.35,
-        character_threshold=0.35,
-        replace_underscores=True,
-        model='SmilingWolf/wd-v1-4-convnextv2-tagger-v2',
-        recursive=False,
-        max_data_loader_n_workers=2,
-        debug=True,
-        undesired_tags=undesired_tags,
-        frequency_tags=False,
-    )
+    asyncio.run(sd_api_wrapper.generate(sd_model=SDModels.sardonyxREDUX_v20, prompt='whale on deep blue sky,white birds,star,thick clouds'))
+    
+    # generate_tags(
+    #     train_data_dir=IMG_DIR,
+    #     caption_extension='.json',
+    #     batch_size=8,
+    #     general_threshold=0.35,
+    #     character_threshold=0.35,
+    #     replace_underscores=True,
+    #     model='SmilingWolf/wd-v1-4-convnextv2-tagger-v2',
+    #     recursive=False,
+    #     max_data_loader_n_workers=2,
+    #     debug=True,
+    #     undesired_tags=undesired_tags,
+    #     frequency_tags=False,
+    # )
 
 
 if __name__ == '__main__':
--- sd_api_wrapper.py
+++ sd_api_wrapper.py
@@ -1,13 +1,14 @@
-import requests, io, base64, os, time
+import requests, base64, time, os, io
 from PIL import Image, PngImagePlugin
 from config import SDModelType, PromptPrefix, SD_URL, IMG_DIR as OUTPUT_DIR
 from custom_logging import setup_logging, log_function_call, log_image_generation_progress
 from typing import Dict, Any, List
+import aiohttp, asyncio
 
 log = setup_logging(debug=True)
 
 
-def _uniquify(path):
+def uniquify(path):
     filename, extension = os.path.splitext(path)
     counter = 1
     while os.path.exists(path):
@@ -15,7 +16,7 @@ def _uniquify(path):
         counter += 1
     return path
 
-def _save_image(json_resp):
+def save_image(json_resp):
     for i in json_resp['images']:
         image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
 
@@ -27,22 +28,22 @@ def _save_image(json_resp):
         
         timestr = time.strftime("%Y%m%d-%H%M%S")
         file_path = os.path.join(OUTPUT_DIR, f'{timestr}.png')
-        file_path = _uniquify(file_path)
+        file_path = uniquify(file_path)
 
         image.save(file_path, pnginfo=pnginfo)
 
-def _add_prefix_to_prompt(prompt: str, prefix: str) -> str:
+def add_prefix_to_prompt(prompt: str, prefix: str) -> str:
     return ", \n\n".join([_ for _ in [prefix, prompt] if _])
 
-def _generate_payloads(
+def generate_payloads(
         model_payload: Dict[str, Any], 
         model_prefixes: PromptPrefix, 
         prompt: str, 
         prompt_n: str, 
         gen_count: int) -> List[Dict[str, Any]]:
     payloads = []
-    positive_prompt = _add_prefix_to_prompt(prompt, model_prefixes.positive)
-    negative_prompt = _add_prefix_to_prompt(prompt_n, model_prefixes.negative)
+    positive_prompt = add_prefix_to_prompt(prompt, model_prefixes.positive)
+    negative_prompt = add_prefix_to_prompt(prompt_n, model_prefixes.negative)
 
     payload = model_payload.copy()  # Make a shallow copy of the model_payload
     payload["prompt"] = positive_prompt
@@ -52,16 +53,35 @@ def _generate_payloads(
 
     return payloads
 
-def _update_model_options(model_option_payload: Dict[str, Any]):
+def update_model_options(model_option_payload: Dict[str, Any]):
     opt = requests.get(url=f'{SD_URL}/sdapi/v1/options')
     opt_json = opt.json()
     new_options = {**opt_json, **model_option_payload}
     requests.post(url=f'{SD_URL}/sdapi/v1/options', json=new_options)
 
-def generate(sd_model: SDModelType, prompt: str, prompt_n: str = None, gen_count: int = 1):
-    _update_model_options(sd_model.option_payload)
+async def request_image(session, data):
+    async with session.post(f'{SD_URL}/sdapi/v1/txt2img', json=data) as response:
+        return await response.json()
+    
+async def check_progress(session):
+    """
+    {
+        "progress": 0,
+        "eta_relative": 0,
+        "state": {},
+        "current_image": "string",
+        "textinfo": "string"
+    }
+    """
+    async with session.get(f'{SD_URL}/sdapi/v1/progress') as response:
+        progress_response = await response.json()
+        progress = progress_response['progress']
+        log.info(f"Progress: {progress}")
 
-    payloads = _generate_payloads(
+async def generate(sd_model: SDModelType, prompt: str, prompt_n: str = None, gen_count: int = 1):
+    update_model_options(sd_model.option_payload)
+
+    payloads = generate_payloads(
         model_payload=sd_model.payload, 
         model_prefixes=sd_model.prompt_prefix, 
         prompt=prompt, 
@@ -74,10 +94,19 @@ def generate(sd_model: SDModelType, prompt: str, prompt_n: str = None, gen_count
     for i, payload in enumerate(payloads):
         start_time = time.time()
 
-        response = requests.post(url=f'{SD_URL}/sdapi/v1/txt2img', json=payload)
-        resp = response.json()
-        _save_image(json_resp=resp)
+        async with aiohttp.ClientSession() as session:
+            # Create the POST task for the image request
+            task_post = asyncio.create_task(request_image(session, payload))
 
+            # Check the progress and sleep every second while waiting for the POST task to complete
+            while not task_post.done():
+                await check_progress(session)
+                await asyncio.sleep(1)
+
+            # Get the result of the POST task
+            resp = await task_post
+
+        save_image(json_resp=resp)
         log_image_generation_progress(i, len(payloads), start_time)
 
     log.info(f"Finished generating images. Saved at: {OUTPUT_DIR}")
