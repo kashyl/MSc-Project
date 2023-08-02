fd0f4dd5905e965bba58f133e7cc9266b37a96cd
Mihai <44536334+kashyl@users.noreply.github.com>
2023-08-02 04:02:01 +0100

    Add GUI generation display

--- gradio_gui.py
+++ gradio_gui.py
@@ -1,13 +1,19 @@
-import numpy as np
 import gradio as gr
+from sd_api_wrapper import generate_image as sd_generate_image
+from config import SDModels
+import asyncio
 
+def generate_image_gradio(x):
+    sd_model = SDModels.sardonyxREDUX_v20
+    prompt = 'whale on deep blue sky, white birds, star, thick clouds'
 
-def generate_image(x):
-    return np.fliplr(x)
+    # Run the async function and wait for it to complete
+    image_with_metadata = asyncio.run(sd_generate_image(sd_model=sd_model, prompt=prompt))
 
+    return image_with_metadata
 
 with gr.Blocks(css="footer {visibility: hidden}") as demo:
-    gr.Markdown("Flip text or image files using this demo.")
+    gr.Markdown("Generate image.")
     with gr.Row():
         text_input = gr.Textbox()
         image_output = gr.Image()
@@ -16,6 +22,6 @@ with gr.Blocks(css="footer {visibility: hidden}") as demo:
     with gr.Accordion("Open for More!"):
         gr.Markdown("Look at me...")
 
-    image_button.click(generate_image, inputs=text_input, outputs=image_output)
+    image_button.click(generate_image_gradio, inputs=text_input, outputs=image_output)
 
 demo.launch()
